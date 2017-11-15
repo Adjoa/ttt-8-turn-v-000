6a2109fe6bbb82075a8812d8840d8169153817aa
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-15 15:06:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,3 +14,19 @@ end
 def move(board, index, character = 'X')
   return board[index] = "#{character}"
 end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  elsif (board[index] == "X" || board[index] == "O")
+    return true
+  end
+end
