ae9dd6fbfd032daf4bbd7298c2f8d65dde2529b0
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-15 15:41:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,9 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   position = gets.strip
+  puts `position is #{position}`
   index = input_to_index(position)
+  puts `Index is #{index}`
   # is_valid = valid_move?(board, position)
   # while !valid_move?(board, position) do
   #   turn(board)
